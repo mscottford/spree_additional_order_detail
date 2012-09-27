a19fa253c7b73eab0e25e3396ec21a83d87c62ec
Jeff Squires <jeff.squires@gmail.com>
2012-09-27 10:45:00 -0400

    gets rid of ActiveSupport::Concern warning about instance method inclusion

--- lib/spree_additional_order_detail/describing.rb
+++ lib/spree_additional_order_detail/describing.rb
@@ -5,15 +5,13 @@ module SpreeAdditionalOrderDetail
     module ClassMethods
     end
  
-    module InstanceMethods
-      def requires_additional_detail?
-        self.additional_order_detail_descriptors.present?
-      end
-
-      def requires_additional_detail_for?(step)
-        self.additional_order_detail_descriptors.where(:model_name => step).exists?
-      end
-    end 
+    def requires_additional_detail?
+      self.additional_order_detail_descriptors.present?
+    end
+    
+    def requires_additional_detail_for?(step)
+      self.additional_order_detail_descriptors.where(:model_name => step).exists?
+    end
 
     included do |base|
       base.has_many :additional_order_detail_descriptors, as: :describable
