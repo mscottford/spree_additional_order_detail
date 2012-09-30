bd286240cb338dec5a0d6cf46b66cca83c0f5a4a
Jeff Squires <jeff.squires@gmail.com>
2012-09-30 18:46:28 -0400

    correctly handles creation errors

--- app/controllers/spree/aircraft_details_controller.rb
+++ app/controllers/spree/aircraft_details_controller.rb
@@ -11,17 +11,17 @@ module Spree
     end
 
     def create
-      line_item_id = params[:line_item_id]
-
       aircraft_detail = AircraftDetail.create(params[:aircraft_detail])
 
-      line_item = Spree::LineItem.find(line_item_id)
+      if aircraft_detail.valid?  # why in the world is create() returning an object and not true false??
+        line_item = Spree::LineItem.find(params[:line_item_id])
 
-      additional_order_detail = line_item.additional_order_details.build
-      additional_order_detail.detailed = aircraft_detail
-      additional_order_detail.line_item = line_item
+        additional_order_detail = line_item.additional_order_details.build
+        additional_order_detail.detailed = aircraft_detail
+        additional_order_detail.line_item = line_item
       
-      additional_order_detail.save!
+        additional_order_detail.save!
+      end
 
       respond_with aircraft_detail
     end
--- app/controllers/spree/vehicle_details_controller.rb
+++ app/controllers/spree/vehicle_details_controller.rb
@@ -11,17 +11,16 @@ module Spree
     end
 
     def create
-      line_item_id = params[:line_item_id]
-
       vehicle_detail = VehicleDetail.create(params[:vehicle_detail])
+      if vehicle_detail.valid?  # why in the world is create() returning an object and not true false??
+        line_item = Spree::LineItem.find(params[:line_item_id])
 
-      line_item = Spree::LineItem.find(line_item_id)
-
-      additional_order_detail = line_item.additional_order_details.build
-      additional_order_detail.detailed = vehicle_detail
-      additional_order_detail.line_item = line_item
+        additional_order_detail = line_item.additional_order_details.build
+        additional_order_detail.detailed = vehicle_detail
+        additional_order_detail.line_item = line_item
       
-      additional_order_detail.save!
+        additional_order_detail.save!
+      end
 
       respond_with vehicle_detail
     end
