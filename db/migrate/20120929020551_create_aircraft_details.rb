50bd1d31c072854505053297ca232f7e4e1129d1
Jeff Squires <jeff.squires@gmail.com>
2012-10-02 12:07:23 -0400

    removes old migrations

--- db/migrate/20120929020551_create_aircraft_details.rb
+++
@@ -1,9 +0,0 @@
-class CreateAircraftDetails < ActiveRecord::Migration
-  def change
-    create_table :aircraft_details do |t|
-      t.integer :year
-      t.string :aircraft_type
-      t.timestamps
-    end
-  end
-end
