33ee80ffd1c956187bb8264781e4ebe29a79aec4
M. Scott Ford <scott@mscottford.com>
2013-02-19 22:06:55 -0500

    Removes spec stubs

--- spec/models/vehicle_detail_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe VehicleDetail do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
--- spec/views/spree/additional_order_detail/index.html.erb_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe "additional_order_detail/index.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
